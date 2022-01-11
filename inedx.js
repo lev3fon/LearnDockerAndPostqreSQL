671f02c9f283021a5fe0b3b8acd168822f2a85af
lev <levtrifonov@mail.ru>
2022-01-11 15:28:03 +0500

    вроде написал 'сервер'

--- inedx.js
+++ inedx.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-//бла
+
+app.get('/time', (req, res) => {
+    const now = new Date();
+    res.send(now);
+    console.log('абоба');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
