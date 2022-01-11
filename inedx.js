3a8a1c74bc579afdc35678fde605e35db2608669
lev <levtrifonov@mail.ru>
2022-01-11 19:13:11 +0500

    добавли неработующие подключение к бд

--- inedx.js
+++ inedx.js
@@ -1,3 +1,4 @@
+const { Sequelize } = require('sequelize');
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -5,9 +6,29 @@ const port = 3000;
 app.get('/time', (req, res) => {
     const now = new Date();
     res.send(now);
-    console.log('абоба');
 })
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 })
+
+
+const sequelize = new Sequelize('database', 'postgres', '12345', {
+    host: "localhost", //your server
+    port: 5432, //server port
+    dialect: 'postgres'
+});
+
+
+
+const testConnection = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('подключился');
+    } catch (error) {
+        console.log(error);
+        console.log('жопень');
+    }
+}
+
+testConnection();
