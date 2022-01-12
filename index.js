93622e70f4910da7693d23ea45dcf0cec621e02f
lev <levtrifonov@mail.ru>
2022-01-12 14:10:14 +0500

    подключил базу, и начал создавать таблицу

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const { Sequelize } = require('sequelize');
+//docker run --name test-pg -p 6432:5432 -e POSTGRES_USER=test-user -e POSTGRES_PASSWORD=123 -e POSTGRES_DB=test-db -d postgres
+//docker run -it --rm -p 5432:5432 --name aboba-pg -e POSTGRES_PASSWORD=12345 postgres:11
+
+
+const { Sequelize, DataTypes } = require('sequelize');
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -12,23 +16,41 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 })
 
+// const sequelize = new Sequelize('postgres://test-user:123@localhost:5432/test-db')
 
 const sequelize = new Sequelize('test-db', 'test-user', '123', {
     host: "localhost", //your server
-    port: 5432, //server port
+    port: 6432, //server port
     dialect: 'postgres'
 });
 
+const RequestLog = sequelize.define('RequestLog', {
+    time: {
+        type: DataTypes.STRING
+    },
+    userAgent: {
+        type: DataTypes.STRING
+    }
+} , {
+    tableName: 'RequestLogs'
+})
 
 
 const testConnection = async () => {
     try {
         await sequelize.authenticate();
         console.log('подключился');
+        return true;
     } catch (error) {
         console.log(error);
         console.log('жопень');
+        return false;
     }
 }
+// console.log(RequestLog == sequelize.models.RequestLog);
 
-testConnection();
+const crreateModel = async () => {
+    if(testConnection()){
+
+    }
+}
