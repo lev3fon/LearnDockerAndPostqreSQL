34af178bb1f32c96ca4d01b9078db4286243b130
lev <levtrifonov@mail.ru>
2022-01-12 15:34:33 +0500

    добавление в таблицу работает

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 //docker run --name test-pg -p 6432:5432 -e POSTGRES_USER=test-user -e POSTGRES_PASSWORD=123 -e POSTGRES_DB=test-db -d postgres
 //docker run -it --rm -p 5432:5432 --name aboba-pg -e POSTGRES_PASSWORD=12345 postgres:11
 
-
 const { Sequelize, DataTypes } = require('sequelize');
 const express = require('express');
 const app = express();
 const port = 3000;
 
 const sequelize = new Sequelize('test-db', 'test-user', '123', {
-    host: "localhost", //your server
-    port: 6432, //server port
+    host: "localhost",
+    port: 6432,
     dialect: 'postgres'
 });
 
 const RequestLog = sequelize.define('RequestLog', {
     time: {
-        type: DataTypes.STRING //DataTypes.DATETIME
+        type: DataTypes.DATE
     },
     userAgent: {
         type: DataTypes.STRING
     }
 } , {
     tableName: 'RequestLogs',
-    // timestamp: false //потом проверь)
+    timestamp: false //НЕ РАБОТАЕТ(((
 })
 
 
@@ -41,7 +40,12 @@ const testConnection = async () => {
 
 const createModel = async () => {
     if(testConnection()){
-        await RequestLog.sync({alter: true});
+
+        try {
+            await RequestLog.sync({alter: true});
+        } catch (error) {
+            await RequestLog.sync({force: true});
+        }
         console.log('(пере)создал таблицу');
     }
 }
@@ -51,14 +55,10 @@ createModel();
 app.get('/time', async (req, res) => {
     console.log('я тут')
     const now = new Date();
-    const newRequestLog = await RequestLog.create({ time: now.toString(), userAgent: req.get('user-agent') });
-    await RequestLog.sync({alter: true});
-    console.log('вроде добавил...');
+    const newRequestLog = await RequestLog.create({ time: now, userAgent: req.get('user-agent') });
     res.send(now);
 })
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 })
-
-// const sequelize = new Sequelize('postgres://test-user:123@localhost:5432/test-db')
