9fd5ddb92907c3ea9b7b1b7cbe3aa59df5583203
lev <levtrifonov@mail.ru>
2022-01-13 21:39:33 +0500

    добавил фаловую структуру

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-const {getConnection} = require('./dbConnection.js')
-const {getRequestLogModel} = require('./requestLogModel.js')
+const { RequestLog } = require('./db-models/index')
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/time', async (req, res) => {
-    const dbConnection = await getConnection()
-    const RequestLog = await getRequestLogModel(dbConnection)
-    console.log('я тут')
     const now = new Date()
     await RequestLog.create({ time: now, userAgent: req.get('user-agent') })
     res.send(now)
